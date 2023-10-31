7593a04689794612edfa476f29e881324b805e29
Beau0405 <beau.thompson81@gmail.com>
2023-10-30 21:05:53 -0500

    class studio

--- classes/studio/ClassStudio.js
+++ classes/studio/ClassStudio.js
@@ -1,7 +1,19 @@
 //Declare a class called CrewCandidate with a constructor that takes three parameters—name, mass, and scores. Note that scores will be an array of test results.
-
-
-
+class CrewCandidate {
+    constructor(name, massKg, scores){
+    this.name = name;
+    this.massKg = massKg;
+    this.scores = scores;
+    } 
+    addScore() {
+        let addScore = addScore.push(scores); 
+    }
+}
+    
+let bubbaBear = new CrewCandidate("Bubba Bear", 135, [88,85,90]);
+let merryMaltice = new CrewCandidate("Merry Maltice", 1.5, [93,88,97]);
+let gladGator = new CrewCandidate("Glad Gator", 225, [75,78,62]);
+console.log(bubbaBear.scores);
 //Add methods for adding scores, averaging scores and determining candidate status as described in the studio activity.
 
 
